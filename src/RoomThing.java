f3a5d5bf5e28dfd608e26310e7a6c0109fb824a9
Jacek <ja77co@wp.pl>
2019-05-09 19:12:35 +0200

    zadanie 3.1 punkt 1.2

--- src/RoomThing.java
+++ src/RoomThing.java
@@ -1,18 +1,11 @@
 public class RoomThing {
-    public static void main (String [] args){
-    Thing thing1 = new Thing();
-    thing1.name = "Zegar";
-    thing1.colour = "biały";
-    thing1.isBroken = true;
-    System.out.println("W moim prokoju jest np. " +thing1.name
-     +", jego kolor jest " +thing1.colour +" i jest " +thing1.isBroken +", że jest zepsuty.");
+    public static void main (String [] args) {
+        Thing thing1 = new Thing("Zegar", "biały", true);
+        System.out.println("W moim prokoju jest np. " + thing1.name
+                + ", jego kolor jest " + thing1.colour + " i jest " + thing1.isBroken + ", że jest zepsuty");
 
-    Thing thing2 = new Thing();
-    thing2.name = "Wieszak";
-    thing2.colour = "czarny";
-    thing2.isBroken = false;
-
-    System.out.println("W moim prokoju jest np. " +thing2.name
-    +", jego kolor jest " +thing2.colour +" i jest " +thing2.isBroken +", że jest zepsuty.");
+        Thing thing2 = new Thing("Wieszak", "czarny", false);
+        System.out.println(" W moim prokoju jest np. " + thing2.name
+                + ", jego kolor jest " + thing2.colour + " i jest " + thing2.isBroken + ", że jest zepsuty");
     }
 }
--- src/Thing.java
+++ src/Thing.java
@@ -2,4 +2,10 @@ public class Thing {
     String name;
     String colour;
     boolean isBroken;
+
+    Thing (String na, String co, boolean iB){
+    name = na;
+    colour = co;
+    isBroken = iB;
+    }
 }
